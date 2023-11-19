45827e5f6be9460d71aae583ffb35d82400fa215
shohag <shohagmiah2100@gmail.com>
2023-11-19 21:27:43 +0600

    api modified

--- app/api/todo/route.js
+++ app/api/todo/route.js
@@ -1,44 +1,44 @@
 import { connectToDB } from "@/components/connectToDb";
 import { Todo } from "@/components/modals";
 
-export const POST = async(req) => {
-    try {
-        const {id} = await req.json();
-        connectToDB()
-        await Todo.findByIdAndDelete(id)
-        return Response.json({message:'success'}, {status:200})
-    } catch (error) {
-        console.log(error)
-        throw new Error(error)
-    }
-}
+export const DELETE = async (req) => {
+  try {
+    const { id } = await req.json();
+    connectToDB();
+    await Todo.findByIdAndDelete(id);
+    return { json: { message: 'success' }, status: 200 };
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
 
-export const PUT = async(req) => {
-    try {
-        const {id,completed} = await req.json();
-        console.log(id)
-        connectToDB()
-        const todo =  await Todo.findById(id);
-        todo.completed = !completed;
-        await todo.save()
-        return Response.json({message:'success'}, {status:200})
-    } catch (error) {
-        console.log(error)
-        throw new Error(error)
-    }
-}
+export const PUT = async (req) => {
+  try {
+    const { id, completed } = await req.json();
+    console.log(id);
+    connectToDB();
+    const todo = await Todo.findById(id);
+    todo.completed = !completed;
+    await todo.save();
+    return { json: { message: 'success' }, status: 200 };
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
 
-export const PATCH = async(req) => {
-    try {
-        const {id,body} = await req.json();
-        console.log(id)
-        connectToDB()
-        const todo =  await Todo.findById(id);
-        todo.body = body;
-        await todo.save()
-        return Response.json({message:'success'}, {status:200})
-    } catch (error) {
-        console.log(error)
-        throw new Error(error)
-    }
-}
+export const PATCH = async (req) => {
+  try {
+    const { id, body } = await req.json();
+    console.log(id);
+    connectToDB();
+    const todo = await Todo.findById(id);
+    todo.body = body;
+    await todo.save();
+    return { json: { message: 'success' }, status: 200 };
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
--- components/singleTodo.jsx
+++ components/singleTodo.jsx
@@ -5,51 +5,47 @@ import { useState } from 'react';
 import { AiFillDelete, AiFillEdit, AiOutlineCheck } from 'react-icons/ai';
 import Modal from './Modal';
 
-const SingleTodo = ({id,completed,body}) => {
-
-  const [open, setOpen] = useState(false)
+const SingleTodo = ({ id, completed, body }) => {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
 
   const closeModal = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
-  const router = useRouter()
-
-  const handleDelete = async() => {
+  const handleDelete = async () => {
     try {
-      await axios.post('api/todo',{id})
-      
+      await axios.delete(`api/todo/${id}`);
+      router.reload(); // Reload instead of refresh
     } catch (error) {
-      console.log('failed to call api');
+      console.error('Failed to call delete API', error);
     }
-    finally{
-      router.refresh()
-    }
-  }
+  };
 
-  const handleComplete = async() => {
+  const handleComplete = async () => {
     try {
-      await axios.put('api/todo',{id,completed})
-      
+      await axios.put(`api/todo/${id}`, { completed: !completed });
+      router.reload(); // Reload instead of refresh
     } catch (error) {
-      console.log('failed to call api');
+      console.error('Failed to call update API', error);
     }
-    finally{
-      router.refresh()
-    }
-  }
+  };
+
+  const toggleModal = () => {
+    setOpen(!open);
+  };
 
   return (
     <>
-            <li className={completed ?"line-through mt-2 flex items-center p-2 text-xl gap-2 border rounded border-gray-700 cursor-pointer" :"flex items-center p-2 text-xl gap-2 mt-2 border rounded border-gray-200 cursor-pointer"}>
-                <span onClick={handleComplete} className='flex-1 text-gray-300'>{body}</span>
-                <buton className='m-2' onClick={() => setOpen(!open)}><AiFillEdit/></buton>
-                <buttton className='m-2' onClick={handleComplete}><AiOutlineCheck /></buttton>
-                <button className='m-2' onClick={handleDelete}><AiFillDelete /></button>
-            </li>
-            {open && <Modal closeModal={closeModal} id={id} body={body}/>}
+      <li className={completed ? "line-through mt-2 flex items-center p-2 text-xl gap-2 border rounded border-gray-700 cursor-pointer" : "flex items-center p-2 text-xl gap-2 mt-2 border rounded border-gray-200 cursor-pointer"}>
+        <span onClick={handleComplete} className='flex-1 text-gray-300'>{body}</span>
+        <button className='m-2' onClick={toggleModal}><AiFillEdit /></button>
+        <button className='m-2' onClick={handleComplete}><AiOutlineCheck /></button>
+        <button className='m-2' onClick={handleDelete}><AiFillDelete /></button>
+      </li>
+      {open && <Modal closeModal={closeModal} id={id} body={body} />}
     </>
-  )
-  }
+  );
+};
 
 export default SingleTodo;
